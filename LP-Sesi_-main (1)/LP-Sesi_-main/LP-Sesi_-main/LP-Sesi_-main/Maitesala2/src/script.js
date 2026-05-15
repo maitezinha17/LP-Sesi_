3fc9a66af49600200efbe2d43955b687d8fb39e1
Maite <maitefranca08@gmail.com>
2026-05-15 11:09:43 -0300

    Delete LP-Sesi_-main (1)/LP-Sesi_-main directory

--- LP-Sesi_-main (1)/LP-Sesi_-main/LP-Sesi_-main/LP-Sesi_-main/Maitesala2/src/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-    const nav = document.querySelector('nav')
-
-    if(window.scrollY > 0) {
-        nav.classList.add('scroll')
-    } else {
-        nav.classList.remove('scroll')
-    }
-}) 
